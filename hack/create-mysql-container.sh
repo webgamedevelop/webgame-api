d7b902abd3309244cceff2e60542009cbcc5a8c0
webgamedevelop <webgamedevelop@163.com>
2024-03-07 10:33:44 +0800

    upgrade mysql version

--- hack/create-mysql-container.sh
+++ hack/create-mysql-container.sh
@@ -4,10 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-docker pull --platform linux/amd64 mysql:5.7.43
+docker pull --platform linux/amd64 mysql:5.7.44
 
 # apt-get install qemu-user-static
 docker run --name mysql -itd -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 \
-    mysql:5.7.43 \
+    mysql:5.7.44 \
     --character-set-server=utf8mb4 \
     --collation-server=utf8mb4_unicode_ci
