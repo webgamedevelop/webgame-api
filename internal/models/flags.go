c0889874107e635c8c98021d56fe0ae28ca7afac
webgamedevelop <webgamedevelop@163.com>
2024-03-14 18:43:03 +0800

    add --gorm-debug-log-level flag

--- internal/models/flags.go
+++ internal/models/flags.go
@@ -9,7 +9,7 @@ import (
 var (
 	address, user, password, database, charset, loc string
 	port                                            uint16
-	parseTime                                       bool
+	parseTime, debugLogLevel                        bool
 )
 
 var commandLine pflag.FlagSet
@@ -23,6 +23,7 @@ func init() {
 	commandLine.StringVar(&charset, "database-charset", "utf8", "database charset")
 	commandLine.StringVar(&loc, "database-loc", "Local", "database parameter loc")
 	commandLine.BoolVar(&parseTime, "database-parseTime", true, "database parameter parseTime")
+	commandLine.BoolVar(&debugLogLevel, "gorm-debug-log-level", false, "set gorm log level to Debug")
 }
 
 // InitFlags is for explicitly initializing the flags.
--- internal/models/models.go
+++ internal/models/models.go
@@ -3,6 +3,7 @@ package models
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
@@ -12,6 +13,14 @@ func Init() error {
 	if err = createDatabase(); err != nil {
 		return err
 	}
+
+	if debugLogLevel {
+		if db, err = gorm.Open(mysql.Open(dsn()), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)}); err != nil {
+			return err
+		}
+		return nil
+	}
+
 	if db, err = gorm.Open(mysql.Open(dsn())); err != nil {
 		return err
 	}
